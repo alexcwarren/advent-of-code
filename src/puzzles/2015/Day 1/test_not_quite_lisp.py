b20b66b39b3fad9b95695d34ea92f326e2784094
Alex Warren <alexander.ch.warren@gmail.com>
2023-10-03 11:05:42 -0400

    Add sample test (2015Day1Part1)

--- src/puzzles/2015/Day 1/test_not_quite_lisp.py
+++ src/puzzles/2015/Day 1/test_not_quite_lisp.py
@@ -9,7 +9,12 @@ def script():
 
 
 def test_sample_input_part1(script):
-    assert script.solve_part1() == 0
+    expected_results = [0, 0, 3, 3, 3, -1, -1, -3, -3]
+    actual_results = script.solve_part1()
+    assert len(expected_results) == len(actual_results)
+
+    for expected, actual in zip(actual_results, expected_results):
+        assert expected == actual
 
 
 def test_sample_input_part2(script):
