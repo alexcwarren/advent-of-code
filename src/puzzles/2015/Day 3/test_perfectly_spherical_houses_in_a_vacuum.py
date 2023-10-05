e459c8be92f9bdd9e058a88c41c8ad6182f2765b
Alex Warren <alexander.ch.warren@gmail.com>
2023-10-05 11:45:05 -0400

    Add sample test (2015Day3Part1)

--- src/puzzles/2015/Day 3/test_perfectly_spherical_houses_in_a_vacuum.py
+++ src/puzzles/2015/Day 3/test_perfectly_spherical_houses_in_a_vacuum.py
@@ -9,7 +9,11 @@ def script():
 
 
 def test_sample_input_part1(script):
-    raise NotImplementedError
+    expected_results = [3, 3, 11]
+    actual_results = script.solve_part1()
+    assert len(expected_results) == len(actual_results)
+    for expected, actual in zip(expected_results, actual_results):
+        assert expected == actual
 
 
 def test_sample_input_part2(script):
