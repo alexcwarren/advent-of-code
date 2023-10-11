ae049612f551972c88e67813bb3a546814302488
Alex Warren <alexander.ch.warren@gmail.com>
2023-10-11 14:42:16 -0400

    Add sample test (2015Day5Part1)

--- src/puzzles/2015/Day 5/test_doesnt_he_have_internelves_for_this.py
+++ src/puzzles/2015/Day 5/test_doesnt_he_have_internelves_for_this.py
@@ -9,7 +9,11 @@ def script():
 
 
 def test_sample_input_part1(script):
-    raise NotImplementedError
+    expected_results = [True, True, False, False, False]
+    actual_results = script.solve_part1()
+    assert len(expected_results) == len(actual_results)
+    for expected, actual in zip(expected_results, actual_results):
+        assert expected == actual
 
 
 def test_sample_input_part2(script):
