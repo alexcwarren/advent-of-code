bc4b5f634bfd9050b9c1e2ef46695b6890565450
Alex Warren <alexander.ch.warren@gmail.com>
2023-10-11 14:32:38 -0400

    Create 2015Day5

---
+++ src/puzzles/2015/Day 5/test_doesnt_he_have_internelves_for_this.py
@@ -0,0 +1,16 @@
+import pytest
+
+import doesnt_he_have_internelves_for_this
+
+
+@pytest.fixture
+def script():
+    return doesnt_he_have_internelves_for_this.DoesntHeHaveInternelvesForThis("sample.in")
+
+
+def test_sample_input_part1(script):
+    raise NotImplementedError
+
+
+def test_sample_input_part2(script):
+    raise NotImplementedError
