12cd6ced90f369b257d9175fa05d2d690f4f34d5
Alex Warren <alexander.ch.warren@gmail.com>
2023-12-04 10:23:59 -0500

    y2023d1p1: Add return value

--- src/puzzles/2023/Day 1/trebuchet.py
+++ src/puzzles/2023/Day 1/trebuchet.py
@@ -34,8 +34,8 @@ class Trebuchet:
         else:
             print(f"{self.solve_part2()}")
 
-    def solve_part1(self):
-        raise NotImplementedError
+    def solve_part1(self) -> int:
+        return 0
 
     def solve_part2(self):
         raise NotImplementedError
